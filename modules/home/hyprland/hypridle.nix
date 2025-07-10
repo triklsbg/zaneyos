ced98a46ab7c1c823aecd68028ecaee7778c7d38
Th. Rikl <workhorse.t@googlemail.com>
2025-07-10 13:29:00 +0200

    timeout of hypridle increased to 9000 from 900

--- modules/home/hyprland/hypridle.nix
+++ modules/home/hyprland/hypridle.nix
@@ -9,14 +9,14 @@
           after_sleep_cmd = "hyprctl dispatch dpms on";
           ignore_dbus_inhibit = false;
           lock_cmd = "hyprlock";
-          };
+        };
         listener = [
           {
-            timeout = 900;
+            timeout = 9000; # in seconds 900 = 15 min
             on-timeout = "hyprlock";
           }
           {
-            timeout = 1200;
+            timeout = 12000;  # was 1200
             on-timeout = "hyprctl dispatch dpms off";
             on-resume = "hyprctl dispatch dpms on";
           }
