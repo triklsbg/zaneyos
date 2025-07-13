584fa90134b0afbe09066e8618aae8ad466fed7f
Th. Rikl <workhorse.t@googlemail.com>
2025-07-13 10:00:40 +0200

    reenabled gnupg.agent.enableSSH

--- modules/core/packages.nix
+++ modules/core/packages.nix
@@ -13,10 +13,9 @@
     fuse.userAllowOther = true;
     mtr.enable = true;
     adb.enable = true;
-    ssh.startAgent = true;   # trikl unclear github.com ssh authorization
     gnupg.agent = {
       enable = true;
-      # enableSSHSupport = true;
+      enableSSHSupport = true;
       pinentryPackage = pkgs.pinentry-qt; # trikl
     };
   };
